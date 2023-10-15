97e8ce6d0adfc6f9dda3901a75c4043f4592763d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-10-15 14:40:07 +0000

    Debugged task 5

--- models/base_model.py
+++ models/base_model.py
@@ -18,15 +18,15 @@ class BaseModel:
         """
 
         if kwargs is not None and kwargs != {}:
-            for key in kwargs:
+            for key, value in kwargs.items():
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    self.__dict__[key] = value
 
         else:
             self.id = str(uuid.uuid4())
